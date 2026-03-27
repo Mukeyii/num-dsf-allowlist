e8ad2af9b6d0998ff4c2a1e22533ca28a00844da
Mukeyii <mk.yildirim.1993@gmail.com>
2026-03-27 12:06:52 +0100

    feat: wire ExpiryBanner, resend verification, and 401 token refresh interceptor

--- frontend/src/pages/AppPage.tsx
+++ frontend/src/pages/AppPage.tsx
@@ -13,6 +13,7 @@ import { CertificateModal } from '../components/modals/CertificateModal';
 import { MembershipModal } from '../components/modals/MembershipModal';
 import { ApprovalModal } from '../components/modals/ApprovalModal';
 import { DownloadModal } from '../components/modals/DownloadModal';
+import { ExpiryWarningBanner } from '../components/layout/ExpiryWarningBanner';
 
 export function AppPage() {
   const activeInstanceId = useCanvasStore((s) => s.activeInstanceId);
@@ -37,6 +38,7 @@ export function AppPage() {
           onDownload={() => openModal('download')}
           onApproval={() => openModal('approval')}
         />
+        <ExpiryWarningBanner />
         {activeInstanceId ? (
           <EntityCanvas instanceId={activeInstanceId} />
         ) : (
